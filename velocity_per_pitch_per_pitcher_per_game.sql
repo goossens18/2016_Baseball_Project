3e5f2f2ccadd1b70cbd8910f5b3e98688db91836
Bryan Goossens <bryangoossens4@gmail.com>
2024-01-24 13:18:20 -0500

    Removed CTE

--- velocity_per_pitch_per_pitcher_per_game.sql
+++ velocity_per_pitch_per_pitcher_per_game.sql
@@ -1,11 +1,9 @@
-with pitchcounts as (
-
-  select pitcherId, gameid, pitchtype,  sum(pitchSpeed) as sumofvelocity, count(pitchspeed) as countofvelocity
-  from bigquery-public-data.baseball.games_wide
-  where pitcherID is not null
-  group by 1,2,3
-
-)
-
-select pitcherID, gameid, pitchtype,sumofvelocity/countofvelocity as averagevelocity
-from pitchcounts
+SELECT 
+  pitcherID,
+  gameid,
+  pitchtype,
+  ROUND(SUM(pitchSpeed) / COUNT(pitchSpeed),2) AS Avg_Game_Velo,
+  ROUND(AVG (SUM(pitchSpeed) / COUNT(pitchSpeed)) OVER (PARTITION BY PITCHTYPE),2) AS Avg_MLBSeason_Velo 
+FROM bigquery-public-data.baseball.games_wide
+WHERE pitcherID IS NOT NULL
+GROUP BY 1, 2, 3;
